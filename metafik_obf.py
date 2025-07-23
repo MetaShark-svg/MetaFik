2ec3dc88a842fc2067e018e2bf3a3aa59fa0bd5a
Shark <108973946+MetaShark-svg@users.noreply.github.com>
2025-07-23 04:36:19 -0400

    dummy locked script metafik_obf.py

---
+++ metafik_obf.py
@@ -0,0 +1,10 @@
+print("""
+🔒 This script is locked.
+
+To unlock full MetaFik functionality:
+➡ Visit https://t.me/MetaFikBot
+➡ Send the command: /buy
+
+After payment, you'll receive the decryption key for the real tool.
+""")
+exit()
